8a69bd1c5362d8bee96624440893220b4abeab70
Aimsucks <contact@aimsucks.space>
2024-08-20 10:39:54 -0400

    feat: implement basic front end and back end

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import SignIn from "@/components/auth/SignIn";
+import { auth } from "../auth";
+import SignOut from "@/components/auth/SignOut";
+
+export default async function Home() {
+  const session = await auth();
+
+  if (!session)
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <SignIn />
+      </main>
+    );
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Hello, {session.user?.name}
+      <br />
+      <SignOut />
+    </main>
+  );
+}
