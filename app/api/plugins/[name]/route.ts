7886bd34e805faa44330254360cf2730eb01c7d7
Aimsucks <contact@aimsucks.space>
2024-09-26 21:34:39 -0400

    feat: add /updates endpoint

--- app/api/plugins/[name]/route.ts
+++ app/api/plugins/[name]/route.ts
@@ -1,103 +1,100 @@
-import { NextResponse } from "next/server";
-import { prisma } from "@/prisma";
-import { Plugin } from "@prisma/client";
-
-export const dynamic = "force-static";
+import {NextRequest, NextResponse} from "next/server";
+import {prisma} from "@/prisma";
 
 const recursiveCategories: any = (level: number) => {
-  if (level === 0) {
-    return {
-      select: {
-        name: true,
-        subcategories: false,
-        presets: {
-          select: {
-            id: true,
-            name: true,
-            description: true,
-            version: true,
-            updatedAt: true,
-            data: true,
-          },
-          orderBy: {
-            name: "asc",
-          },
-        },
-      },
-      orderBy: {
-        name: "asc",
-      },
-    };
-  }
+    if (level === 0) {
+        return {
+            select: {
+                name: true,
+                subcategories: false,
+                presets: {
+                    select: {
+                        id: true,
+                        name: true,
+                        description: true,
+                        version: true,
+                        updatedAt: true,
+                        data: true,
+                    },
+                    orderBy: {
+                        name: "asc",
+                    },
+                },
+            },
+            orderBy: {
+                name: "asc",
+            },
+        };
+    }
 
-  return {
-    select: {
-      name: true,
-      subcategories: recursiveCategories(level - 1),
-      presets: {
+    return {
         select: {
-          id: true,
-          name: true,
-          description: true,
-          version: true,
-          updatedAt: true,
-          data: true,
+            name: true,
+            subcategories: recursiveCategories(level - 1),
+            presets: {
+                select: {
+                    id: true,
+                    name: true,
+                    description: true,
+                    version: true,
+                    updatedAt: true,
+                    data: true,
+                },
+                orderBy: {
+                    name: "asc",
+                },
+            },
         },
         orderBy: {
-          name: "asc",
+            name: "asc",
         },
-      },
-    },
-    orderBy: {
-      name: "asc",
-    },
-  };
+    };
 };
 
 export async function GET(
-  request: Request,
-  { params }: { params: { name: string } }
+    request: NextRequest,
+    { params }: { params: { name: string } }
 ) {
-  // Find plugin by name, case insensitive
-  // Recursively goes down up to 3 subcategories (so category -> sub -> sub -> sub)
-  // TODO: Prevent creating categories past 3 subs
-  const plugin = await prisma.plugin.findFirst({
-    where: { name: { equals: params.name, mode: "insensitive" } },
-    select: {
-      id: true,
-      name: true,
-      description: true,
-      categories: {
+    // Find plugin by name, case insensitive
+    // Recursively goes down up to 3 subcategories (so category -> sub -> sub -> sub)
+    // TODO: Prevent creating categories past 3 subs
+    const plugin = await prisma.plugin.findFirst({
+        where: { name: { equals: params.name, mode: "insensitive" } },
         select: {
-          name: true,
-          subcategories: recursiveCategories(2),
-          presets: {
-            select: {
-              id: true,
-              name: true,
-              description: true,
-              version: true,
-              updatedAt: true,
-              data: true,
+            id: true,
+            name: true,
+            description: true,
+            categories: {
+                select: {
+                    name: true,
+                    subcategories: recursiveCategories(2),
+                    presets: {
+                        select: {
+                            id: true,
+                            name: true,
+                            description: true,
+                            version: true,
+                            updatedAt: true,
+                            data: true,
+                        },
+                        orderBy: {
+                            name: "asc",
+                        },
+                    },
+                },
+                where: {
+                    parentCategoryId: null,
+                },
+                orderBy: {
+                    name: "asc",
+                },
             },
-            orderBy: {
-              name: "asc",
-            },
-          },
         },
-        where: {
-          parentCategoryId: null,
-        },
-        orderBy: {
-          name: "asc",
-        },
-      },
-    },
-  });
+    });
 
-  // Return entirety of plugin data if the plugin exists
-  if (plugin) return NextResponse.json(plugin, { status: 200 });
+    // Return entirety of plugin data if the plugin exists
+    if (plugin) return NextResponse.json(plugin, { status: 200 });
 
-  // Return 404 if no plugin was found by the provided name
-  return NextResponse.json({ message: "Plugin not found." }, { status: 404 });
+    // Return 404 if no plugin was found by the provided name
+    return NextResponse.json({ message: "Plugin not found." }, { status: 404 });
 }
---
+++ app/api/plugins/[name]/updates/route.ts
@@ -0,0 +1,29 @@
+import {NextRequest, NextResponse} from "next/server";
+import {prisma} from "@/prisma";
+
+export async function GET(request: NextRequest) {
+    const searchParams: URLSearchParams = request.nextUrl.searchParams
+    const query: string | null = searchParams.get("query")
+
+    console.log(query)
+
+    if (!query) {
+        return NextResponse.json({message: "No query parameters provided."}, {status: 400})
+    }
+
+    const ids: number[] = query.split(",").map(id => parseInt(id))
+
+    const presets = await prisma.preset.findMany({
+        where: {id: {in: ids}},
+        select: {
+            id: true,
+            name: true,
+            version: true,
+            updatedAt: true
+        }
+    })
+
+    if (presets) return NextResponse.json(presets, {status: 200});
+
+    return NextResponse.json({message: "Plugin not found."}, {status: 404});
+}
