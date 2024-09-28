0cbee44aef387db6fcef2da4dd36fa985bd0cafd
Aimsucks <contact@aimsucks.space>
2024-09-28 17:12:07 -0400

    refactor: implement new route for presets

--- app/api/v1/plugins/[name]/route.ts
+++ app/api/v1/plugins/[name]/route.ts
@@ -1,94 +1,16 @@
 import {NextRequest, NextResponse} from "next/server";
 import {prisma} from "@/prisma";
 
-const recursiveCategories: any = (level: number) => {
-    if (level === 0) {
-        return {
-            select: {
-                name: true,
-                subcategories: false,
-                presets: {
-                    select: {
-                        id: true,
-                        name: true,
-                        description: true,
-                        version: true,
-                        updatedAt: true,
-                        data: true,
-                    },
-                    orderBy: {
-                        name: "asc",
-                    },
-                },
-            },
-            orderBy: {
-                name: "asc",
-            },
-        };
-    }
-
-    return {
-        select: {
-            name: true,
-            subcategories: recursiveCategories(level - 1),
-            presets: {
-                select: {
-                    id: true,
-                    name: true,
-                    description: true,
-                    version: true,
-                    updatedAt: true,
-                    data: true,
-                },
-                orderBy: {
-                    name: "asc",
-                },
-            },
-        },
-        orderBy: {
-            name: "asc",
-        },
-    };
-};
-
 export async function GET(
     request: NextRequest,
     { params }: { params: { name: string } }
 ) {
-    // Find plugin by name, case insensitive
-    // Recursively goes down up to 3 subcategories (so category -> sub -> sub -> sub)
-    // TODO: Prevent creating categories past 3 subs
     const plugin = await prisma.plugin.findFirst({
         where: { name: { equals: params.name, mode: "insensitive" } },
         select: {
             id: true,
             name: true,
-            description: true,
-            categories: {
-                select: {
-                    name: true,
-                    subcategories: recursiveCategories(2),
-                    presets: {
-                        select: {
-                            id: true,
-                            name: true,
-                            description: true,
-                            version: true,
-                            updatedAt: true,
-                            data: true,
-                        },
-                        orderBy: {
-                            name: "asc",
-                        },
-                    },
-                },
-                where: {
-                    parentCategoryId: null,
-                },
-                orderBy: {
-                    name: "asc",
-                },
-            },
+            description: true
         },
     });
 
